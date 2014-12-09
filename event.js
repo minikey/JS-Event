d90c11a6b15e11a476a1050a875d6dea59165692
FMin <635548528@qq.com>
2014-12-09 18:34:53 +0800

    Update event.js

--- event.js
+++ event.js
@@ -1,42 +1,63 @@
-(function (window) {
-	var EventCore = function () {};
-	EventCore.prototype = {
-		on: function(types, listener) {
-			types = types.split(/\s+/);
-			for (var i=0, ty; ty = types[i++];) {
-				getList(this, ty).push(listener);
-			}
-		},
+(function(window) {
+    var EventCore = function() {};
+    EventCore.prototype = {
+        on: function(types, listener) {
+            types = getTypeArray(types);
+            for (var i = 0, ty; ty = types[i++];) {
+                getList(this, ty).push(listener);
+            }
+        },
+        off: function(types, listener) {
+            var list, i = 0, k, l, ty, c, tmp = [];
+            types = getTypeArray(types);
+            while (ty = types[i++]) {
+                list = getList(this, ty);
+                tmp = [];
+                for (k = 0, l = list.length; c = list[k++];) {
+                    if (c !== listener) {
+                        tmp[tmp.length] = c;
+                    }
+                }
+                setList(this, ty, tmp);
+            }
+        },
+        trigger: function(types, data) {
+            var list, i = 0,
+                k = 0,
+                len, ty;
+            types = getTypeArray(types);
+            while (ty = types[i++]) {
+                list = getList(this, ty);
+                for (len = list.length; k < len; k++) {
+                	list[k].apply(this, data);
+                }
+            }
+        }
+    };
 
-		off: function(types, listener) {
-			types = types.split(/\s+/);
-			var list, k, l, tmp = [];
-			for (var i=0, ty; ty = types[i++];) {
-				list = getController(this, ty);
-				tmp = [];
-				for (k = 0, l = list.length, c; c = list[k++];) {
-					if(c !== listener) {
-						tmp[tmp.length] = c;
-					}
-				}
-				
-			}
-		}
-	};
+    function getTypeArray(types) {
+        return types.split(/\s+/);
+    }
 
-	function getList (obj, type) {
-		var controller = obj.__controller;
-		controller = controller || (obj.__controller = {});
+    function getList(obj, type) {
+        var controller = obj.__controller;
+        controller = controller || (obj.__controller = {});
+		controller[type] = controller[type] || [];     
+        return controller[type];
+    }
 
-		return controller[type] || (controller[type] = []);
-	}
+    function setList(obj, type, list) {
+        obj.__controller[type] = list;
+    }
 
-	// EXPOSE
-	if (typeof define === 'function' && define.amd) {
-		define(function() {return EventCore;});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = EventCore;
-	} else {
-		window.EventCore = EventCore;
-	}
+    // EXPOSE
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return EventCore;
+        });
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = EventCore;
+    } else {
+        window.EventCore = EventCore;
+    }
 })(window);
